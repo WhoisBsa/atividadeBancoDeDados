382b4564dbf25b89b9172f628e95cb4c527f35a4
HLAM <hemilioaraujo@gmail.com>
2019-12-10 21:04:15 -0300

    renomado diretório principal

--- src/atividadeBancoDeDados/atividadeBancoDeDados.java
+++ src/atividadeBancoDeDados/atividadeBancoDeDados.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package LoginUserCLientBD;
+package atividadeBancoDeDados;
 
 import Views.*;
 
@@ -11,7 +11,7 @@ import Views.*;
  *
  * @author matheus
  */
-public class LoginBd {
+public class atividadeBancoDeDados {
 
     /**
      * @param args the command line arguments
